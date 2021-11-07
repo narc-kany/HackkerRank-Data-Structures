7e1a0661755cbd6dd9811c5867d7e55cf2a872c2
Sivanesh S <86925605+narc-kany@users.noreply.github.com>
2021-11-07 22:06:14 +0530

    Left Rotation expercise
    Sample Input
    
    5 4
    1 2 3 4 5
    Sample Output
    
    5 1 2 3 4

---
+++ DS/Left Rotation.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, arr):
+    # Write your code here
+    # left rotate arr with count d (d is number of hops)
+    # d: means from dth element to end of the array
+    # :d mens to split it from right of the array
+    arr = arr[d:] + arr[:d]
+    return arr
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
