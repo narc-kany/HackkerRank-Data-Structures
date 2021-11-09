60d65fec67d375ae0c817323feaa29663797f8b7
Sivanesh S <86925605+narc-kany@users.noreply.github.com>
2021-11-10 02:47:41 +0530

    Print In Reverse
    Input (stdin)
    
    Download
    3
    5
    16
    12
    4
    2
    5
    3
    7
    3
    9
    5
    5
    1
    18
    3
    13
    Expected Output
    
    Download
    5
    2
    4
    12
    16
    9
    3
    7
    13
    3
    18
    1
    5

---
+++ DS/PrintInReverse.py
@@ -0,0 +1,65 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep):
+    while node:
+        print(node.data, end='')
+
+        node = node.next
+
+        if node:
+            print(sep, end='')
+
+#
+# Complete the 'reversePrint' function below.
+#
+# The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def reversePrint(llist):
+    # Write your code here
+    # print it in reverse
+    # check head if null if null do nothing (base case)
+    if llist == None:
+        return
+    reversePrint(llist.next)
+    # return value continously until the entire loop is executed and at the end head becomes none and this craziness will end 
+ # refer 49 int data is there in the SinglyLinkedListNode:
+    print(llist.data)
+
+if __name__ == '__main__':
