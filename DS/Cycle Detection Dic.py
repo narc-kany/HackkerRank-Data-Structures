d4eb0f4571361d10ee0b5a87fd06f88865abfe2e
Sivanesh S <86925605+narc-kany@users.noreply.github.com>
2021-11-13 21:00:00 +0530

    Cycle Detection using Dictionary (time linear but space complexity)
    Input (stdin)
    
    Download
    1
    -1
    1
    1
    Expected Output
    
    Download
    0

---
+++ DS/Cycle Detection Dic.py
@@ -0,0 +1,60 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the has_cycle function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def has_cycle(head):
+    # using dictionary to solve it alternative 
+    dic = {} # initiate the dictionary variable
+    _ = head # create a temp node for head
+    while _: # check the node is not empty 
+        dic[_] = 1
+        if dic.get(_.next, 0) !=0: # check the range until the loop is completed
+            return True
+        _ = _.next
+    return False
+
+if __name__ == '__main__':
